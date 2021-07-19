db88ec5a602868e4d26df02c23d4b23ffc01e71c
Hanno Braun <hanno@braun-odw.eu>
2021-07-19 16:28:17 +0000

    Model spacer

--- models/spacer-libfive/src/main.rs
+++ models/spacer-libfive/src/main.rs
@@ -2,14 +2,19 @@ use libfive::{BRepSettings, Region3, Tree, TreeVec3};
 
 fn main() {
     let outer_diameter = 30.0;
+    let inner_diameter = 12.0;
     let height = 10.0;
 
-    // TASK: Model actual spacer.
     let spacer = Tree::cylinder_z(
         (outer_diameter / 2.0).into(),
         height.into(),
         TreeVec3::new(0.0, 0.0, 0.0),
-    );
+    )
+    .difference(Tree::cylinder_z(
+        (inner_diameter / 2.0).into(),
+        height.into(),
+        TreeVec3::new(0.0, 0.0, 0.0),
+    ));
 
     let xy_max = outer_diameter * 1.1;
     let z_min = -height * 0.1;
