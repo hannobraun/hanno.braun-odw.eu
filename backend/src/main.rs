cfb3105d6e277bb327adaf3fc1583cc907ac979f
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 12:55:40 +0000

    Initialize logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,8 @@ use warp::Filter as _;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt().init();
+
     let hello = warp::any().map(|| "Hello, world!");
     warp::serve(hello).run((Ipv6Addr::UNSPECIFIED, 8000)).await;
 }
