acf65d3f34dd057b00a387e4f263e3ca492c9b91
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 12:56:33 +0000

    Log each request

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,6 +6,8 @@ use warp::Filter as _;
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let hello = warp::any().map(|| "Hello, world!");
+    let hello = warp::any()
+        .map(|| "Hello, world!")
+        .with(warp::trace::request());
     warp::serve(hello).run((Ipv6Addr::UNSPECIFIED, 8000)).await;
 }
