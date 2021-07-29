cefda1e040fde1f67f8bc489c2d27b168f51f2ed
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 13:40:34 +0000

    Extend install script

--- backend/install.sh
+++ backend/install.sh
@@ -3,4 +3,9 @@
 set -e
 
 cargo build --release
+
 cp target/release/backend /usr/local/bin
+cp backend.service /usr/local/lib/systemd/system
+
+systemctl enable backend.service
+systemctl start backend.service
