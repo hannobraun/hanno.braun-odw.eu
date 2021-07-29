7e47debc45bbe0c25658e7a4dc163d4098a0ff67
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 15:45:20 +0000

    Move HTTPS server setup code to dedicated function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,7 @@
 use std::{net::Ipv6Addr, path::PathBuf};
 
 use clap::Clap;
-use warp::{Filter as _, Reply as _};
+use warp::{Filter as _, Future, Reply as _};
 
 #[tokio::main]
 async fn main() {
@@ -11,6 +11,25 @@ async fn main() {
     let https_port = args.https_port.unwrap_or(8443);
     let serve_dir = args.serve.unwrap_or("static".into());
 
+    https_server(serve_dir, https_port).await;
+}
+
+/// Custom backend for made-by.braun-odw.eu
+#[derive(Clap)]
+struct Args {
+    /// Port to listen on. Will listen on port 8443, if omitted.
+    #[clap(short, long)]
+    https_port: Option<u16>,
+
+    /// Static file directory to serve. Defaults to `static`, if omitted.
+    #[clap(short, long)]
+    serve: Option<PathBuf>,
+}
+
+fn https_server(
+    serve_dir: impl Into<PathBuf> + 'static,
+    https_port: u16,
+) -> impl Future {
     let hello = warp::fs::dir(serve_dir)
         .map(|file: warp::fs::File| {
             if file.path().ends_with("hello") {
@@ -27,17 +46,4 @@ async fn main() {
         .key_path("tls/localhost.key.pem")
         .cert_path("tls/localhost.cert.pem")
         .run((Ipv6Addr::UNSPECIFIED, https_port))
-        .await;
-}
-
-/// Custom backend for made-by.braun-odw.eu
-#[derive(Clap)]
-struct Args {
-    /// Port to listen on. Will listen on port 8443, if omitted.
-    #[clap(short, long)]
-    https_port: Option<u16>,
-
-    /// Static file directory to serve. Defaults to `static`, if omitted.
-    #[clap(short, long)]
-    serve: Option<PathBuf>,
 }
