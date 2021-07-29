f7340e2981e677223fc09f760b595cac20b3d060
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 13:03:15 +0000

    Parse command-line arguments

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,20 @@
 use std::net::Ipv6Addr;
 
+use clap::Clap;
 use warp::Filter as _;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
 
+    Args::parse();
+
     let hello = warp::any()
         .map(|| "Hello, world!")
         .with(warp::trace::request());
     warp::serve(hello).run((Ipv6Addr::UNSPECIFIED, 8000)).await;
 }
+
+/// Custom backend for made-be.braun-odw.eu
+#[derive(Clap)]
+struct Args;
