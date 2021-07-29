6f8f9b4fe2d1d2cea022ec53bc5c46eb7c5fbbc7
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 15:43:33 +0200

    Refactor

--- backend/install.sh
+++ backend/install.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+UNIT_DIR=/usr/local/lib/systemd/system
+
 cargo build --release
 
 cp target/release/backend /usr/local/bin
-cp backend.service /usr/local/lib/systemd/system
+cp backend.service $UNIT_DIR
 
 systemctl enable backend.service
 systemctl start backend.service
