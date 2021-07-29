f8dc303a2122d27ef111245598cf4eee82cfdce6
Hanno Braun <hanno@braun-odw.eu>
2021-07-29 12:38:29 +0000

    Convert `backend` into HTTP server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,9 @@
+use std::net::Ipv6Addr;
+
+use warp::Filter as _;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let hello = warp::any().map(|| "Hello, world!");
+    warp::serve(hello).run((Ipv6Addr::UNSPECIFIED, 8000)).await;
 }
