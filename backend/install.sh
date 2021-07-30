244d7efa03b8e77062ae67644479fa175582a447
Hanno Braun <hanno@braun-odw.eu>
2021-07-30 15:42:43 +0000

    Build and deploy Zola website in install script

--- backend/install.sh
+++ backend/install.sh
@@ -11,8 +11,10 @@ cp -f target/release/backend /usr/local/bin
 mkdir -p $UNIT_DIR
 cp backend.service $UNIT_DIR
 
+(cd zola; zola build)
 rm -r $SERVE_DIR
-cp -r static $SERVE_DIR
+mkdir -p $SERVE_DIR
+cp -r zola/public/* static/* -t $SERVE_DIR
 
 systemctl enable backend.service
 systemctl restart backend.service
