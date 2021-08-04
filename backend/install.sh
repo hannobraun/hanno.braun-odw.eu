15d5e30ec00129cf39395dde2976470eb0c15348
Hanno Braun <hanno@braun-odw.eu>
2021-08-04 13:01:02 +0000

    Update install script

--- backend/install.sh
+++ backend/install.sh
@@ -3,7 +3,9 @@
 set -e
 
 UNIT_DIR=/usr/local/lib/systemd/system
-STATIC_DIR=/srv/made-by.braun-odw.eu/static
+SERVE_DIR=/srv/made-by.braun-odw.eu
+STATIC_DIR=$SERVE_DIR/static
+ZOLA_DIR=$SERVE_DIR/zola
 
 cargo build --release
 sudo cp -f target/release/backend /usr/local/bin
@@ -13,7 +15,10 @@ sudo cp backend.service $UNIT_DIR
 sudo systemctl enable backend.service
 sudo systemctl restart backend.service
 
+sudo rm -rf $SERVE_DIR
+sudo mkdir -p $SERVE_DIR
+
+sudo cp -r static/ $STATIC_DIR
+
 (cd zola; zola build)
-sudo rm -rf $STATIC_DIR
-sudo mkdir -p $STATIC_DIR
-sudo cp -r zola/public/* static/* -t $STATIC_DIR
+sudo cp -r zola/public/ $ZOLA_DIR
