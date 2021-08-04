46134d2e14aa63621e7a7114d0a01e1fdb822424
Hanno Braun <hanno@braun-odw.eu>
2021-08-04 12:53:48 +0000

    Merge `CliArgs` into `Args`

--- backend/src/args.rs
+++ backend/src/args.rs
@@ -2,31 +2,9 @@ use std::path::PathBuf;
 
 use clap::Clap;
 
-pub struct Args {
-    pub http_port: u16,
-    pub https_port: u16,
-    pub tls_key: PathBuf,
-    pub tls_cert: PathBuf,
-    pub static_dir: PathBuf,
-}
-
-impl Args {
-    pub fn parse() -> Self {
-        let args = CliArgs::parse();
-
-        Self {
-            http_port: args.http_port,
-            https_port: args.https_port,
-            tls_key: args.tls_key,
-            tls_cert: args.tls_cert,
-            static_dir: args.static_dir,
-        }
-    }
-}
-
 /// Custom backend for made-by.braun-odw.eu
 #[derive(Clap)]
-pub struct CliArgs {
+pub struct Args {
     /// HTTP port to listen on
     #[clap(long, default_value = "8080")]
     pub http_port: u16,
@@ -46,4 +24,10 @@ pub struct CliArgs {
     /// Path to static files
     #[clap(long, default_value = "static")]
     pub static_dir: PathBuf,
+}
+
+impl Args {
+    pub fn parse() -> Self {
+        <Self as Clap>::parse()
+    }
 }
