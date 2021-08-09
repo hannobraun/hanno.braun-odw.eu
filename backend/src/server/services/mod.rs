b112623abb3a163ca17870866e8cbed7d9223073
Hanno Braun <hanno@braun-odw.eu>
2021-08-09 13:16:56 +0000

    Adapt module name

--- backend/src/server/services/mod.rs
+++ backend/src/server/services/mod.rs
@@ -1,12 +1,12 @@
 mod redirect_home;
 mod redirect_legacy_domain;
-mod redirect_legacy_path;
+mod redirect_path_prefix;
 mod redirect_to_https;
 mod serve_static;
 
 pub use self::{
     redirect_home::redirect_home,
     redirect_legacy_domain::redirect_legacy_domain,
-    redirect_legacy_path::redirect_path_prefix,
+    redirect_path_prefix::redirect_path_prefix,
     redirect_to_https::redirect_to_https, serve_static::serve_static,
 };
