4c7205464fd18671550a24095fcd7b21cc6935e8
Hanno Braun <hanno@braun-odw.eu>
2021-08-10 13:32:14 +0000

    Resize images on demand

--- backend/src/server/services/serve_static.rs
+++ backend/src/server/services/serve_static.rs
@@ -1,8 +1,13 @@
 use std::path::PathBuf;
 
-use warp::{Filter, Rejection, Reply};
+use serde::Deserialize;
+use warp::{
+    http::{Response, StatusCode},
+    hyper::Body,
+    reply, Filter, Rejection, Reply,
+};
 
-use crate::server::util::handle_not_found;
+use crate::server::util::{handle_not_found, resize_image};
 
 pub fn serve_static(
     static_dir: impl Into<PathBuf> + 'static,
@@ -10,5 +15,32 @@ pub fn serve_static(
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     warp::fs::dir(static_dir)
         .or(warp::fs::dir(zola_dir))
+        .unify()
+        .and(warp::query::<ImageArgs>())
+        .map(|file: warp::fs::File, args: ImageArgs| {
+            if let Some(width) = args.width {
+                if file.path().as_os_str().to_string_lossy().ends_with(".jpg") {
+                    match resize_image(file.path(), width) {
+                        Ok(image) => {
+                            return Response::new(Body::from(image));
+                        }
+                        Err(err) => {
+                            return reply::with_status(
+                                err.to_string(),
+                                StatusCode::INTERNAL_SERVER_ERROR,
+                            )
+                            .into_response();
+                        }
+                    }
+                }
+            }
+
+            file.into_response()
+        })
         .recover(handle_not_found)
+}
+
+#[derive(Debug, Deserialize)]
+struct ImageArgs {
+    width: Option<u32>,
 }
