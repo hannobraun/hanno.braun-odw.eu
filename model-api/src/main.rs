53bd7fe0336102bf92a27bae0aa7378c0755f111
Hanno Braun <hanno@braun-odw.eu>
2021-08-18 12:35:35 +0000

    Add web server

--- model-api/src/main.rs
+++ model-api/src/main.rs
@@ -1,4 +1,19 @@
+use std::net::Ipv6Addr;
+
+use axum::{
+    handler::get, response::IntoResponse, route, routing::RoutingDsl as _,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app = route("/", get(hello_world));
+    axum::Server::bind(&(Ipv6Addr::UNSPECIFIED, 8080).into())
+        .serve(app.into_make_service())
+        .await
+        // TASK: Improve error handling.
+        .unwrap()
+}
+
+async fn hello_world() -> impl IntoResponse {
+    "Hello, world!"
 }
