68c22c8d04971d2fc8511b34c9e9f0cf4f87b0e1
Hanno Braun <hanno@braun-odw.eu>
2021-08-20 14:13:27 +0000

    Add route for spacer

--- model-api/src/main.rs
+++ model-api/src/main.rs
@@ -2,10 +2,15 @@ use rocket::{get, routes};
 
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, spacer])
 }
 
 #[get("/hello")]
 fn hello() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/model/spacer?<outer>&<inner>&<height>")]
+fn spacer(outer: f64, inner: f64, height: f64) -> String {
+    format!("outer: {}, inner: {}, height: {}", outer, inner, height)
 }
