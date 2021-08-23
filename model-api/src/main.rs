00776b4d173e64be042b0812564d240e00cb0bed
Hanno Braun <hanno@braun-odw.eu>
2021-08-23 14:12:25 +0000

    Handle errors from OpenSCAD

--- model-api/src/main.rs
+++ model-api/src/main.rs
@@ -1,4 +1,7 @@
-use rocket::{get, routes, Responder};
+use rocket::{
+    error, get, http::Status, response::Responder as RocketResponder, routes,
+    Responder,
+};
 use thiserror::Error;
 use tokio::{io, process::Command};
 
@@ -9,8 +12,7 @@ fn rocket() -> _ {
 
 #[get("/model/spacer?<outer>&<inner>&<height>")]
 async fn spacer(outer: f64, inner: f64, height: f64) -> Result<String, Error> {
-    // TASK: Check status code.
-    Command::new("openscad")
+    let status = Command::new("openscad")
         .arg(format!("-Douter={}", outer))
         .arg(format!("-Dinner={}", inner))
         .arg(format!("-Dheight={}", height))
@@ -21,6 +23,12 @@ async fn spacer(outer: f64, inner: f64, height: f64) -> Result<String, Error> {
         .status()
         .await?;
 
+    // This can use `ExitStatus::exit_ok`, once that is stabilized.
+    if !status.success() {
+        // TASK: Add OpenSCAD output to error.
+        return Err(Error::OpenScad(OpenScadError));
+    }
+
     // TASK: Serve generated file.
     Ok(format!("Call to OpenSCAD succeeded"))
 }
@@ -29,4 +37,20 @@ async fn spacer(outer: f64, inner: f64, height: f64) -> Result<String, Error> {
 enum Error {
     #[error("I/O error")]
     Io(#[from] io::Error),
+
+    #[error("Error calling OpenSCAD")]
+    OpenScad(OpenScadError),
+}
+
+#[derive(Debug)]
+struct OpenScadError;
+
+impl<'r> RocketResponder<'r, 'static> for OpenScadError {
+    fn respond_to(
+        self,
+        _: &'r rocket::Request,
+    ) -> rocket::response::Result<'static> {
+        error!("Error calling OpenSCAD");
+        Err(Status::InternalServerError)
+    }
 }
