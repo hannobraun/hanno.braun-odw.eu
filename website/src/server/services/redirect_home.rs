a5190b52720d75c67a67f069a5bb9408be3e8034
Hanno Braun <hanno@braun-odw.eu>
2021-08-24 11:17:22 +0000

    Remove website service

--- website/src/server/services/redirect_home.rs
+++
@@ -1,8 +0,0 @@
-use warp::{http::Uri, Filter, Rejection, Reply};
-
-use crate::server::util::redirect;
-
-pub fn redirect_home(
-) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    warp::path::end().map(|| redirect::temporary(Uri::from_static("/updates")))
-}
--- website/src/server/util/filter_not_applicable.rs
+++
@@ -1,4 +0,0 @@
-#[derive(Debug)]
-pub struct FilterNotApplicable;
-
-impl warp::reject::Reject for FilterNotApplicable {}
