4dd39efa6732fdcfe95ea4be4b4ad92149168973
Hanno Braun <hanno@braun-odw.eu>
2021-08-25 10:32:03 +0000

    Generate title and date

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,18 @@
-use time::Instant;
+use time::{macros::format_description, OffsetDateTime};
 
 fn main() {
-    let now = Instant::now();
-    println!("Now: {:?}", now);
+    // Date and time formats follow ISO 8601. See Wikipedia:
+    // See https://en.wikipedia.org/wiki/ISO_8601.
+    let title_format =
+        format_description!("[year][month][day]T[hour][minute][second]Z");
+    let date_format = format_description!("[year]-[month]-[day]");
+
+    let now = OffsetDateTime::now_utc();
+
+    // TASK: Improve error handling.
+    let title = now.format(&title_format).unwrap();
+    let date = now.format(&&date_format).unwrap();
+
+    println!("Title: {}", title);
+    println!("Date: {}", date);
 }
