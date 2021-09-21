377f2feec7f49314e06e962a7471d3ec2785a574
Hanno Braun <hanno@braun-odw.eu>
2021-09-21 09:17:48 +0000

    Add command-line arguments to CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,24 @@
 mod update;
 
+use clap::Clap;
+
 fn main() -> anyhow::Result<()> {
-    update::write()?;
+    let args = Args::parse();
+
+    match args.command {
+        Command::Update => update::write()?,
+    }
 
     Ok(())
+}
+
+#[derive(Clap)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Clap)]
+enum Command {
+    Update,
 }
