ea2b1bc24c203a44f7761b916487911d8b8755a0
Hanno Braun <hanno@braun-odw.eu>
2021-09-21 09:12:28 +0000

    Move update writing code to dedicated module

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,55 +1,7 @@
-use std::{
-    fs::{self, OpenOptions},
-    io::Write as _,
-    path::Path,
-};
-
-use time::{macros::format_description, OffsetDateTime};
+mod update;
 
 fn main() -> anyhow::Result<()> {
-    // Date and time formats follow ISO 8601. See Wikipedia:
-    // See https://en.wikipedia.org/wiki/ISO_8601.
-    let title_format =
-        format_description!("[year][month][day]T[hour][minute][second]Z");
-    let date_format = format_description!("[year]-[month]-[day]");
-
-    let now = OffsetDateTime::now_utc();
-
-    let title = now.format(&title_format)?;
-    let date = now.format(&&date_format)?;
-
-    let dir_path = format!("content/updates/{}", title);
-    fs::create_dir_all(&dir_path)?;
-
-    let file_path = Path::new(&dir_path).join("index.md");
-    let mut update = OpenOptions::new()
-        .create_new(true)
-        .write(true)
-        .open(file_path)?;
-
-    write!(
-        update,
-        "\
-+++
-title = \"{title}\"
-date  = \"{date}\"
-+++
-
-{{{{
-    preview_image(
-        path=\"updates/{title}/image.jpg\",
-        alt=\"\"
-    )
-}}}}
-
-{{{{ ext_link(url=\"https://example.com\", text=\"An external link.\") }}}}
-
-This is an update.
-\
-        ",
-        title = title,
-        date = date,
-    )?;
+    update::write()?;
 
     Ok(())
 }
