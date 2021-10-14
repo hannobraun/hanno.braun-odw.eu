6d48c76d596a2cb69d7a02e2c16bdd19163f992c
Hanno Braun <hanno@braun-odw.eu>
2021-10-14 11:47:34 +0000

    Use different method to pin Deno version

--- website/scripts/build.sh
+++ website/scripts/build.sh
@@ -2,17 +2,7 @@
 
 set -e
 
-# Required because `brew tap-new` initializes a Git repository.
-git config --global user.name "Hanno Braun"
-git config --global user.email "hanno@braun-odw.eu"
-
-TAP=hannobraun/deno
-MODULE=deno
-VERSION=1.14.3
-
-brew tap-new $TAP
-brew extract --version $VERSION $MODULE $TAP
-brew install $TAP/$MODULE@$VERSION
+curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.0.0
 
 zola build
 deno run -A https://deno.land/x/lume/ci.ts
