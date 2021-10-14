2188517e4d16c1f309ce570b536dcbeba8c4789e
Hanno Braun <hanno@braun-odw.eu>
2021-10-14 11:12:38 +0000

    Use different method to pin Deno version

--- website/scripts/build.sh
+++ website/scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-brew install deno@1.14.3
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/d77989e0193c1c9dd9aa462d4179d11653e41ca5/Formula/deno.rb
 
 zola build
 deno run -A https://deno.land/x/lume/ci.ts
